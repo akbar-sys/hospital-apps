93f0bf41c8a1cdfc8a349e8a66c698514cc14bce
SaktiDw <sakti596@gmail.com>
2020-05-13 08:57:30 +0800

    Done

--- src/router/index.js
+++ src/router/index.js
@@ -41,9 +41,14 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
-  scrollBehavior () {
+  scrollBehavior (_to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
       return { x: 0, y: 0 }
+    }
   }
+  
   
 })
 
